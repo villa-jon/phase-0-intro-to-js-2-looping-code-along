cc86b429cd7f3ae09aab44f3d84c798f76dc28d2
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-18 14:13:22 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'birthday';
+
+const newArr = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++)
+    {
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger; 
+} return newArr; 
+}
+
+
+const numbers = ['1','2','3','4','5','6','7','8','9','10'];
+function countDown(init = 10){
+    while (init>=0) {
+        console.log(init--);
+    }
+}
