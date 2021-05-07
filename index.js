678f835b169a922eb29a6474917fd973c3f67a91
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-05-07 00:53:20 -0700

    redo

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ function countDown(init = 10){
         console.log(init--);
     }
 }
+// Hello J again, so for the sake of my own sanity of not totally redo-ing work (It's not that I'm lazy, but...)
+// I'm going to explain the lab. So at the top we have our array. 
+// so for the first test, what I am doing is creating a new array and then putting in two 
+// parameters (names,event). We are using a for loop to loop the array for 
+// each name that is in the array. Then we are pushing the new array 
+// through a looped sentence. So that each person is able to get a thank you.
+// Oringinally I wanted to Wanda's family, but that is beside the point.
+// 
+// For the last two tests, what I'm doing here is declaring a new array
+// that is counting down to 0. So first IU am init the array at the variable "10",
+// while using a "while" loop. We are then asking while the positive numbers are greater than 
+// 0 (starting from ten), we are console.logging down by 1 (init--).
